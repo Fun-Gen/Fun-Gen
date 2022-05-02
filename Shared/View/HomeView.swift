a68a8c01c7d0a2c71adacd8aa9ac8ae403c7c3e0
Finnigan Johnson <finnj00@uw.edu>
2022-05-02 16:54:27 -0700

    Initial HomeView and added test activities

---
+++ Shared/View/HomeView.swift
@@ -0,0 +1,43 @@
+//
+//  HomeView.swift
+//  Fun Gen
+//
+//  Created by Finnigan Johnson on 4/25/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    var activities: [Activity] = []
+    
+    var body: some View {
+        
+        NavigationView {
+            List {
+                ForEach(activities) { activity in
+                    NavigationLink(destination: Text(activity.title)) {
+                        Text(activity.title)
+                    }
+                }
+            }
+            .navigationTitle("FunGen")
+            .toolbar {
+                Button(action: {
+                    print("hi")
+                }, label: {
+                    Image(systemName: "plus")
+                })
+            }
+        }
+    }
+    
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            HomeView(activities: testActivities)
+        }
+    }
+}
