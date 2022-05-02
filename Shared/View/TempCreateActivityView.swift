4fdff892770b3a95787368a7b69f4e5b93598a95
elulu3 <elena.wu2000@gmail.com>
2022-05-02 15:34:48 -0700

    added to create activity page

--- Shared/View/TempCreateActivityView.swift
+++ Shared/View/TempCreateActivityView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct TempCreateActivityView: View {
     @State private var selectedCategory: Category = .outdoor
+    @State var optionList: [String] = []
+    @State var newOption = ""
+    @State var friendList: [String] = []
+    @State var newFriend = ""
 
     var body: some View {
         VStack(alignment: .leading) {
@@ -26,6 +30,32 @@ struct TempCreateActivityView: View {
                     }
                 }
             }.padding()
+            
+            Text("Options")
+                .font(.title).padding()
+            ForEach(optionList, id: \.self) { item in
+                Button(item) {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }.padding(5)
+            }.padding(.leading)
+            TextField("Suggest an option", text: $newOption) {
+                self.optionList.append(self.newOption)
+                self.newOption = ""
+            }.padding()
+            
+            Group {
+                Text("Friends Tagged")
+                    .font(.title)
+                    .padding()
+                ForEach(friendList, id: \.self) { item in
+                    Text(item).padding(5)
+                }.padding(.leading)
+                TextField("Name", text: $newFriend) {
+                    self.friendList.append(self.newFriend)
+                    self.newFriend = ""
+                }.padding()
+            }
+            
             Spacer()
             HStack {
                 Spacer()
