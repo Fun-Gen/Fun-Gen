66a06fd1097b9f8cc58454d332ffd768b74749a4
aratta <aratta@cs.washington.edu>
2022-05-03 19:38:52 -0700

    Address Failed to scroll to visible (by AX action) Button?

--- Fun GenUITests/Fun_GenUITests.swift
+++ Fun GenUITests/Fun_GenUITests.swift
@@ -7,6 +7,19 @@
 
 import XCTest
 
+// Had an issue with Failed to scroll to visible (by AX action) Button, stackoverflow solution
+extension XCUIElement {
+    func forceTapElement() {
+        if self.isHittable {
+            self.tap()
+        }
+        else {
+            let coordinate: XCUICoordinate = self.coordinate(withNormalizedOffset: CGVector(dx:0.0, dy:0.0))
+            coordinate.tap()
+        }
+    }
+}
+
 class Fun_GenUITests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -31,17 +44,20 @@ class Fun_GenUITests: XCTestCase {
     }
     
     // UI test for frontend, adding an option on the VoteView page
+    
+   
     func testAddOption() throws {
         let app = XCUIApplication()
         app.launch()
-        
         app.buttons["Movie Night"].tap()
         app.scrollViews.otherElements.textFields["Suggest an option"].tap()
         app.scrollViews.otherElements.textFields["Suggest an option"].typeText("Spider Man")
-        app/*@START_MENU_TOKEN@*/.keyboards.buttons["Return"]/*[[".keyboards",".buttons[\"return\"]",".buttons[\"Return\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[2,0]]@END_MENU_TOKEN@*/.tap()
+        app.buttons["return"].forceTapElement()
         XCTAssert(app.scrollViews.otherElements.buttons["Spider Man"].exists)
         app.scrollViews.otherElements.buttons["Spider Man"].tap()
     }
+    
+    
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
