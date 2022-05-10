29ba632f9f9adcd1b9f6965c07d61411d9e7fbf3
aratta <aratta@cs.washington.edu>
2022-05-09 17:18:57 -0700

    VoteView lines

--- Shared/View/VoteView.swift
+++ Shared/View/VoteView.swift
@@ -10,26 +10,48 @@ import SwiftUI
 struct VoteView: View {
     @EnvironmentObject var user: UserViewModel
     @EnvironmentObject var activity: ActivityViewModel
-    
+    @EnvironmentObject var option: OptionViewModel
+
     @State private var optionArray = ["Shrek", "Wall-E", "ET"]
     @State private var isSelected = ""
     @State private var newOption = ""
-    
-    // TODO: VoteView retrieves title, category, and options by using activity(id: Activity.ID) from ActivityViewModel
+        
+    // Some default values, will automatically be replaced when call to .activity() method is made
+    @State private var act = Activity(id: "1", title: "Movie Night", category: Category.movie, author: "1")
+    // VoteView retrieves title, category by using activity(id: Activity.ID) from ActivityViewModel
     var body: some View {
         VStack(alignment: .leading) {
-            Text("Activity").font(.title).padding(.bottom)
+            // Get an activity to retrieve data (title, category, options)
+            // Not sure how to resolve Static method 'buildBlock' requires
+            // that 'Task<(), Never>' conform to 'View' without using Button...
+            Button {
+                Task {
+                    do {
+                        act = try await ActivityViewModel.activity(id: "AhdjSLOlTJqb68aXBAWn")
+                    } catch {
+                        print(error)
+                    }
+                }
+            } label: {
+                NavigationLink(destination: VoteView()) {
+                    Text("Get Activity")
+                }
+            }
             
             // Testing purposes
-            Text("activityid \(activity.activity?.id ?? "")")
+            // Text("Activity \(act.title)")
+            // Text("Activity").font(.title).padding(.bottom)
             
-            // Text(tempActivty.title).font(.title3)
-            // Text("Category: \(tempActivty.category.rawValue.capitalized)").foregroundColor(.secondary)
+            // Testing purposes for retrieving the corresponding activityID?
+            // Text("activityid \(activity.activity?.id ?? "")")
+            
+            Text("Activity \(act.title)").font(.title3)
+            Text("Category: \(act.category.rawValue.capitalized)").foregroundColor(.secondary)
             Text("Options").font(.title).padding(.top)
             // Option selection
             ScrollView {
                 VStack(alignment: .leading) {
-                    // TODO: Correctly update fields from Model files when user inputs data
+                    // TODO: Loop through the activity options array...
                     ForEach(optionArray, id: \.self) { item in
                         Button {
                             self.isSelected = item
@@ -60,6 +82,7 @@ struct VoteView: View {
 
 struct VoteView_Previews: PreviewProvider {
     static var previews: some View {
-        VoteView().environmentObject(UserViewModel()).environmentObject(ActivityViewModel(activityID: "1"))
+        // TODO: Need to create ActicityID and OptionID, store in variable, and pass it in here
+        VoteView().environmentObject(UserViewModel()).environmentObject(ActivityViewModel(activityID: "AhdjSLOlTJqb68aXBAWn")).environmentObject(OptionViewModel(optionID: "B5uYuFf31aEqH5sGHFA4"))
     }
 }
