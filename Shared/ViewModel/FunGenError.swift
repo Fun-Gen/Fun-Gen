a0877ef11943cc84d82383368a7e317e847d0878
Apollo Zhu <zhuzhiyu@cs.washington.edu>
2022-05-17 04:25:00 -0700

    Implement and test random selection
    - Add random selection
    - Add veto for testing
    - Add test for randomly selecting an option
    
    Relax initial check for concurrent testing
    
    
    Test veto through repeated runs instead

--- Shared/ViewModel/FunGenError.swift
+++ Shared/ViewModel/FunGenError.swift
@@ -7,5 +7,16 @@
 
 import Foundation
 
-enum FunGenError: Error {
+enum FunGenError: LocalizedError {
+    case activityHasNoOptionForRandomSelection
+    case activityAlreadyHasSelectedOption
+    
+    var errorDescription: String? {
+        switch self {
+        case .activityAlreadyHasSelectedOption:
+            return "The activity already has a selected option."
+        case .activityHasNoOptionForRandomSelection:
+            return "The activity has no option to randomly select from."
+        }
+    }
 }
