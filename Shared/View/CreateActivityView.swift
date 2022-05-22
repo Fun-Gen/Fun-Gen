aba796270fda7a758aa63861f795778b926fe912
Finnigan Johnson <finnj00@uw.edu>
2022-05-22 13:20:25 -0700

    Moved Create button and added scrolling to CreateActivity

--- Shared/View/CreateActivityView.swift
+++ Shared/View/CreateActivityView.swift
@@ -18,59 +18,59 @@ struct CreateActivityView: View {
     @State var newFriend = ""
     @State private var title: String = ""
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Create Activity").font(.title)
-            TextField("Enter in activity title", text: $title)
-            HStack {
-                Text("Select Category:")
-                // Loop through the category enum in Category.swift using a picker
-                Picker("Category", selection: $selectedCategory) {
-                    ForEach(Category.allCases, id: \.self) { category in
-                        Text(category.rawValue.capitalized)
-                    }
-                }
-            }
-            Group {
-                Text("Options")
-                    .font(.title)
-                ForEach(optionList, id: \.self) { item in
-                    Text(item)
-                }
-                TextField("Suggest an option", text: $newOption) {
-                    self.optionList.append(self.newOption)
-                    self.newOption = ""
-                }
-            }
-            Text("Friends Tagged")
-                .font(.system(.title2))
-            ForEach(friendList, id: \.self) { item in
-                Text(item)
-            }
-            TextField("Name", text: $newFriend) {
-                self.friendList.append(self.newFriend)
-                self.newFriend = ""
-            }
-            HStack {
-                Spacer()
-                Button(action: {
-                    Task {
-                        do {
-                            _ = try await ActivityViewModel.createActivity(
-                                title: title, // Ice Cream Outing
-                                category: selectedCategory, // Food
-                                author: "\(user.user?.id ?? "")", // author: "eOUU1RDjcphzXd0VTUDhALy6ZB53"
-                                optionTitles: optionList, // Mint, Choco, Straw
-                                additionalMembers: []
-                            ) // Might leave off friends tagging for beta?
-                        } catch {
-                            // TODO: handle error
-                            print(error)
+        ScrollView {
+            VStack(alignment: .leading) {
+                Text("Create Activity").font(.title)
+                TextField("Enter in activity title", text: $title)
+                HStack {
+                    Text("Select Category:")
+                    // Loop through the category enum in Category.swift using a picker
+                    Picker("Category", selection: $selectedCategory) {
+                        ForEach(Category.allCases, id: \.self) { category in
+                            Text(category.rawValue.capitalized)
                         }
                     }
-                    self.presentationMode.wrappedValue.dismiss()
-                }, label: { Text("Create") })
-            }
-        }.padding()
+                }
+                Group {
+                    Text("Options")
+                        .font(.title)
+                    ForEach(optionList, id: \.self) { item in
+                        Text(item)
+                    }
+                    TextField("Suggest an option", text: $newOption) {
+                        self.optionList.append(self.newOption)
+                        self.newOption = ""
+                    }
+                }
+                Text("Friends Tagged")
+                    .font(.system(.title2))
+                ForEach(friendList, id: \.self) { item in
+                    Text(item)
+                }
+                TextField("Name", text: $newFriend) {
+                    self.friendList.append(self.newFriend)
+                    self.newFriend = ""
+                }
+            }.padding()
+        }.toolbar {
+            Button(action: {
+                Task {
+                    do {
+                        _ = try await ActivityViewModel.createActivity(
+                            title: title, // Ice Cream Outing
+                            category: selectedCategory, // Food
+                            author: "\(user.user?.id ?? "")", // author: "eOUU1RDjcphzXd0VTUDhALy6ZB53"
+                            optionTitles: optionList, // Mint, Choco, Straw
+                            additionalMembers: []
+                        ) // Might leave off friends tagging for beta?
+                    } catch {
+                        // TODO: handle error
+                        print(error)
+                    }
+                }
+                self.presentationMode.wrappedValue.dismiss()
+            }, label: { Text("Create") })
+        }
     }
 }
 
