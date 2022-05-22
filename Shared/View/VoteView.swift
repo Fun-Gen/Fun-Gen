5703fee19f2e53ba1f1aecf2fd5ae40fc2afa713
Finnigan Johnson <finnj00@uw.edu>
2022-05-22 14:46:22 -0700

    Added unvoting and vote switching

--- Shared/View/VoteView.swift
+++ Shared/View/VoteView.swift
@@ -9,15 +9,17 @@ import SwiftUI
 
 struct VoteView: View {
     @EnvironmentObject var activityViewModel: ActivityViewModel
+    @EnvironmentObject var userViewModel: UserViewModel
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @State private var optionArray = ["OP1", "OP2", "OP3"]
     @State private var isSelected = ""
+    @State private var selectedOption = ""
     @State private var newOption = ""
     
     var sortedOptionIDs: [Option.ID] {
         activityViewModel.activity?.options.keys.sorted() ?? []
     }
-    
+        
     var body: some View {
         VStack(alignment: .leading) {
             if let activity = activityViewModel.activity {
@@ -29,9 +31,12 @@ struct VoteView: View {
                 ScrollView {
                     VStack(alignment: .leading) {
                         ForEach(sortedOptionIDs, id: \.self) { optionID in
-                            VoteOptionView(optionID: optionID,
-                                           activity: activity,
-                                           optionViewModel: OptionViewModel(optionID: optionID))
+                            VoteOptionView(
+                                select: changeSelected,
+                                optionID: optionID,
+                                activity: activity,
+                                optionViewModel: OptionViewModel(optionID: optionID)
+                            )
                         }
                         TextField("Suggest an option", text: $newOption) {
                                 optionArray.append(self.newOption)
@@ -55,11 +60,47 @@ struct VoteView: View {
             }
         }.navigationTitle("Vote").padding()
     }
+    
+    func changeSelected(newID: Option.ID) async {
+        do {
+            guard let userID = userViewModel.user?.id else {
+                return
+            }
+            guard let activity = activityViewModel.activity else {
+                return
+            }
+            if selectedOption.isEmpty {
+                    try await ActivityViewModel
+                        .changeVote(ofUser: userID,
+                                    addTo: newID,
+                                    inActivity: activity.id)
+                    selectedOption = newID
+            } else if selectedOption == newID {
+                    try await ActivityViewModel
+                        .changeVote(ofUser: userID,
+                                    removeFrom: newID,
+                                    inActivity: activity.id)
+                    selectedOption = ""
+            } else {
+                    try await ActivityViewModel
+                        .changeVote(ofUser: userID,
+                                    removeFrom: selectedOption,
+                                    addTo: newID,
+                                    inActivity: activity.id)
+                    selectedOption = newID
+            }
+        } catch {
+            // TODO: Handle Error
+            print("Error")
+        }
+        return
+    }
 }
 
 struct VoteView_Previews: PreviewProvider {
     static var previews: some View {
         VoteView()
             .environmentObject(ActivityViewModel(activityID: "AhdjSLOlTJqb68aXBAWn"))
+            .environmentObject(UserViewModel())
     }
 }
