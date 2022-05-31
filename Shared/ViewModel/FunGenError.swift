36b1385733127f1c072036a9ad39e24555a4ccba
j-cse <103292325+j-cse@users.noreply.github.com>
2022-05-30 18:44:22 -0700

    End vote (#67)
    * voteCount is now being stored in DB for both Activity and PollOption. changeVote() now properly increments/decrements the voteCount
    
    * fixed Activity voteCount
    
    * added endVote() function
    
    * selectedOption field is now updated in endVote()
    
    * throws errors in endVote() instead of returning empty string
    
    Co-authored-by: keys <keys@Air.local>

--- Shared/ViewModel/FunGenError.swift
+++ Shared/ViewModel/FunGenError.swift
@@ -10,6 +10,8 @@ import Foundation
 enum FunGenError: LocalizedError {
     case activityHasNoOptionForRandomSelection
     case activityAlreadyHasSelectedOption
+    case activityNoLongerContainsThisOption
+    case activityDoesNotHaveAllVotesIn
     
     var errorDescription: String? {
         switch self {
@@ -17,6 +19,10 @@ enum FunGenError: LocalizedError {
             return "The activity already has a selected option."
         case .activityHasNoOptionForRandomSelection:
             return "The activity has no option to randomly select from."
+        case .activityNoLongerContainsThisOption:
+            return "The Fun Gen'd option is no longer apart of this Activity."
+        case .activityDoesNotHaveAllVotesIn:
+            return "There are still members who have not voted."
         }
     }
 }
